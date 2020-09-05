45df747a4a2f01fc4c16a354291e9b44ba3da93a
ThomasJorna <thomasfkjorna@gmail.com>
2020-09-05 16:38:30 +0200

    Updates

---
+++ precache-manifest.4544a41e845ec83960bc298849a1c3b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c27c26462c8dc3e6f6568cc5460f118",
+    "url": "/dutch-language-course/index.html"
+  },
+  {
+    "revision": "3ba6f78be56ce41a06cb",
+    "url": "/dutch-language-course/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "1bb7c8afef2cb9fe7bd7",
+    "url": "/dutch-language-course/static/css/main.2aa2e89d.chunk.css"
+  },
+  {
+    "revision": "3ba6f78be56ce41a06cb",
+    "url": "/dutch-language-course/static/js/2.53fe983c.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/dutch-language-course/static/js/2.53fe983c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb7c8afef2cb9fe7bd7",
+    "url": "/dutch-language-course/static/js/main.3e849150.chunk.js"
+  },
+  {
+    "revision": "1cf3f81eff88986b1c6c",
+    "url": "/dutch-language-course/static/js/runtime-main.6abd1b0a.js"
+  }
+]);
