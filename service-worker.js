96fdfd2e230e2c03bd750689e38b99c65f5d09e6
ThomasJorna <thomasfkjorna@gmail.com>
2020-09-19 19:37:25 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dutch-language-course/precache-manifest.cde90d12ef24611b55db31b70b30d816.js"
+  "/dutch-language-course/precache-manifest.9b0922b5130f1775dac6098d80f1ce99.js"
 );
 
 self.addEventListener('message', (event) => {
