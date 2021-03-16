ab3da08c4d8a7874f0a80cfe28518562ac0220a7
Thomas F. K. Jorna <thomasfkjorna@gmail.com>
2021-03-16 19:53:17 +0100

    Updates

---
+++ precache-manifest.05fd0d89b4b7fa3300d8e71209317c91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db752db251de545f049a645ed8d82ff8",
+    "url": "/dutch-language-course/index.html"
+  },
+  {
+    "revision": "4ae886591ed06f295a8b",
+    "url": "/dutch-language-course/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "2240e8cfaf6cc2c5bfc4",
+    "url": "/dutch-language-course/static/css/main.83030bc2.chunk.css"
+  },
+  {
+    "revision": "4ae886591ed06f295a8b",
+    "url": "/dutch-language-course/static/js/2.336d9cff.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/dutch-language-course/static/js/2.336d9cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2240e8cfaf6cc2c5bfc4",
+    "url": "/dutch-language-course/static/js/main.ae77d635.chunk.js"
+  },
+  {
+    "revision": "1cf3f81eff88986b1c6c",
+    "url": "/dutch-language-course/static/js/runtime-main.6abd1b0a.js"
+  },
+  {
+    "revision": "b640140e53588b7f5ac0c6a8c1a3e042",
+    "url": "/dutch-language-course/static/media/correct.b640140e.mp3"
+  },
+  {
+    "revision": "11f2ef09d03ebe51745c4f831edbb029",
+    "url": "/dutch-language-course/static/media/incorrect.11f2ef09.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dutch-language-course/precache-manifest.8c5f76b361bbbcca6f2bf5a353919a27.js"
+  "/dutch-language-course/precache-manifest.05fd0d89b4b7fa3300d8e71209317c91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dutch-language-course/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
